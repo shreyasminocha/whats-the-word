59a09c50ba400b43acd462649aac891917542e34
SeventhPrize <SeventhPrize@users.noreply.github.com>
2021-09-18 16:42:40 -0500

    testing

--- Notes_Trainer.py
+++ Notes_Trainer.py
@@ -1,8 +1,21 @@
 import Clustering
 import numpy as np
 
-def get_all_vectors(embed_array):
+def get_all_vectors(embed_list):
     '''
-    array of array of bert_embed vectors
+    list of bert_embed arrays
     '''
+    for sheet in (embed_list):
+        english_sentences = np.array(english_sentences_input, copy=True)
+        bert_embed = np.array(bert_embed_input, copy=True)
+
+
+        n_cluster_max = 100
+        cluster = Clustering(self.bert_embed)
+        n_cluster, kmeans = cluster.compute_optimal_custer(n_cluster_max)
+        sentence_ind = cluster.get_central_sentences(n_cluster, kmeans)
+        
+
+
+
     
