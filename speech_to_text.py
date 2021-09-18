79d420f689d510379386cad4cf4164e5677a01b2
SeventhPrize <SeventhPrize@users.noreply.github.com>
2021-09-18 17:13:50 -0500

    Testing

--- speech_to_text.py
+++ speech_to_text.py
@@ -4,12 +4,20 @@ from google.cloud import speech
 # Instantiates a client
 client = speech.SpeechClient()
 
-media_uri = ""
+media_uri = "gs://lecture_audio_files/Class 1, Part 2 - Economic Growth Theory and the Direct Elements in Innovation-n0QLcw-CHmk.mp3"
 
-audio = speech.RecognitionAudio(uri=media_uri)
+audio_aud = speech.RecognitionAudio(uri=media_uri)
 
-config = speech.RecognitionConfig(
+config_aud = speech.RecognitionConfig(
     encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
     sample_rate_hertz=16000,
-    language_code="en-US",
+    language_code="en-US"
 )
+
+operation = client.recognize(
+    config=config_aud,
+    audio=audio_aud
+)
+
+response = operation.result()
+print(response)
