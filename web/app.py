ed7a52523289f8374136eed4a9c3e63e5461cd07
Shreyas Minocha <sm-git@riseup.net>
2021-09-18 17:13:56 -0500

    Save form submissions

--- web/app.py
+++ web/app.py
@@ -1,6 +1,13 @@
+import os
+
 from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTS = ['srt', 'txt']
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = '/tmp'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 @app.route('/', methods=['GET'])
 def index():
@@ -8,14 +15,21 @@ def index():
 
 @app.route('/notes', methods=['GET', 'POST'])
 def notes():
-	#notes = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+	file = request.files['transcript']
+
+	if file.filename == '':
+		return 400
+
+	filename = secure_filename(file.filename)
+	file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 	notes = ['The following content is provided under a Creative Commons license',
- 'Your support will help MIT OpenCourseWare continue to offer high quality educational resources for free',
- 'To make a donation or to view additional materials from hundreds of MIT courses, visit MIT OpenCourseWare at ocw',
- 'mit',
- 'edu',
- "WILLIAM BONVILLIAN: So today, we really kind of talk more about the system, the kind of third direct innovation factor, and then about these indirect factors that are important and significant, but not as powerful, I'd argue, as the direct factors"
+		'Your support will help MIT OpenCourseWare continue to offer high quality educational resources for free',
+		'To make a donation or to view additional materials from hundreds of MIT courses, visit MIT OpenCourseWare at ocw',
+		'mit',
+		'edu',
+		"WILLIAM BONVILLIAN: So today, we really kind of talk more about the system, the kind of third direct innovation factor, and then about these indirect factors that are important and significant, but not as powerful, I'd argue, as the direct factors"
 	]
 
 	return render_template('notes.html', notes=notes)
