7cc7cc0d9b992d0c91db91c19d7af0e3f4bda026
Shreyas Minocha <sm-git@riseup.net>
2021-09-18 12:04:38 -0500

    Render index page

--- web/app.py
+++ web/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'test'
+	return render_template('index.html')
 
 @app.route('/transcript')
 def transcript():
