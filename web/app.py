637b17bbede2714776ba16bbb9e7f846c2f51792
Shreyas Minocha <sm-git@riseup.net>
2021-09-18 13:39:20 -0500

    Fix form submission workflow

--- web/app.py
+++ web/app.py
@@ -2,13 +2,12 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
 	return render_template('index.html')
 
-@app.route('/transcript')
-def transcript():
-	if request.method != 'POST':
-		return 405
+@app.route('/notes', methods=['GET', 'POST'])
+def notes():
+	notes = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-	return 'foo'
+	return render_template('notes.html', notes=notes)
