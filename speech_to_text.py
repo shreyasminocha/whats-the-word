5f80ac35ae26a9cab737038edbe4d245ea2d2609
SeventhPrize <SeventhPrize@users.noreply.github.com>
2021-09-18 16:42:40 -0500

    testing

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,2 +1,15 @@
 import os
 from google.cloud import speech
+
+# Instantiates a client
+client = speech.SpeechClient()
+
+media_uri = ""
+
+audio = speech.RecognitionAudio(uri=media_uri)
+
+config = speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code="en-US",
+)
