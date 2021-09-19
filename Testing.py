3f11a26c7ef9a2d87f5ad95ef9e5c2e1fb6a3042
SeventhPrize <SeventhPrize@users.noreply.github.com>
2021-09-18 19:45:01 -0500

    Functional

--- Testing.py
+++ Testing.py
@@ -9,3 +9,4 @@ summary = Bert_Summarizer(sentences_list, bert_encode)
 sentence_ind, kmeans, n_cluster = summary.get_indices()
 print(sentence_ind, n_cluster)
 notes = summary.indices_to_english(sentence_ind, toPrint=True)
+
