5e4c7e1e0b3fbc9308a8e7705ba6028c6c3f3a5e
SeventhPrize <SeventhPrize@users.noreply.github.com>
2021-09-18 19:45:17 -0500

    Functional

--- Output.py
+++ Output.py
@@ -13,4 +13,4 @@ class Output:
         bert_encode, sentences_list, lines, timestamps = bert.run()
         summary = Bert_Summarizer(sentences_list, bert_encode)
         sentence_ind, kmeans, n_cluster = summary.get_indices()
-        return summary.indices_to_english(sentence_ind, toPrint=True)
+        return summary.indices_to_english(sentence_ind, toPrint=False)
